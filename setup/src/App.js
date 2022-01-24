62e887958b599af4e8ad87e243709eb938350330
oryaakov2 <85134352+oryaakov2@users.noreply.github.com>
2022-01-24 20:20:21 +0200

    first commit

---
+++ setup/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+
+function App() {
+
+  return (
+    <main>
+      <div className='container'>
+        <h3>Questions And Answers About Login</h3>
+        <section className='info'>
+          {data.map((question) => (
+            <SingleQuestion key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App;
